4e341d9392ae06e85147f4bf16a0748defb2077f
aparnaboddu <111386689+aparnaboddu@users.noreply.github.com>
2022-08-16 16:35:27 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,5 @@ streamlit.text('biryani')
 streamlit.text('avacado toast')
 streamlit.header('🍕 kids make your own pizza🍕')
 import pandas
-
+my_fruit_list=pandas.read_csv(" https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
