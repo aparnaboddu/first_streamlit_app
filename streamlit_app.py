760d1c8823de07de016ac59386d83144a2425acb
aparnaboddu <111386689+aparnaboddu@users.noreply.github.com>
2022-08-16 16:05:17 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('my kids are champions')
+streamlit.title('breakfast menu')
+streamlit.title('bread omlet')
+streamlit.title('rice lunch')
+streamlit.title('snack')
